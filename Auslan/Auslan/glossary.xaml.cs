278f0db8f7e6fb1282e827830cd229fec88c5ad8
Mitchell <c3132270@uon.edu.au>
2018-08-28 22:25:32 +1000

    Add project files.

---
+++ Auslan/Auslan/glossary.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Auslan
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class glossary : ContentPage
+	{
+		public glossary ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Alphabet(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new alphabet());
+        }
+    }
+}
